6695593c7d14c727851ca675289dd7a620240915
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-03-12 17:49:36 -0300

    (strings) find a string

---
+++ Strings/find_a_string.py
@@ -0,0 +1,19 @@
+# Hacker Rank Find a string exercice
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+def count_substring(string, sub_string):
+    count = 0
+
+    i = string.find(sub_string)
+
+    while i != -1:
+        count += 1
+        i = string.find(sub_string, i+1)
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
