ff4cafed60d3397f43347881a9e606e6a007fae9
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-03-13 14:44:09 -0300

    (sets) union operation

---
+++ Sets/union_operation.py
@@ -0,0 +1,11 @@
+# Hacker Rank Union operation exercice
+# https://www.hackerrank.com/challenges/py-set-union/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+en_newspaper = input().split()
+b = int(input())
+fr_newspaper = input().split()
+
+print(len(set(en_newspaper) | set(fr_newspaper)))
