341c8c86c6f0d8115a7ff66bab5c1aed83ae4f30
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-03-13 17:57:09 -0300

    (preparation kit) lonely integer

---
+++ Preparation kits/1 week/lonely_integer.py
@@ -0,0 +1,36 @@
+# Hacker Rank Lonely integer exercice
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    for el in a:
+        if a.count(el) == 1:
+            return el
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
