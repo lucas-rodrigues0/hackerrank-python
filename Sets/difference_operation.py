bc1f9a5d8b3d02eaa4f66ed3c566f1817ed92329
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-03-13 14:48:56 -0300

    (sets) difference operation

---
+++ Sets/difference_operation.py
@@ -0,0 +1,11 @@
+# Hacker Rank Difference operation exercice
+# https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+en_newspaper = input().split()
+b = int(input())
+fr_newspaper = input().split()
+
+print(len(set(en_newspaper) - set(fr_newspaper)))
